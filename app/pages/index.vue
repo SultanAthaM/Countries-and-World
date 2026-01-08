26371fa3aae9952e346120d53df21402a490932f
mrsam <natureson111111@gmail.com>
2026-01-09 01:52:49 +0700

    Fix accessibility

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -53,6 +53,10 @@ const filteredCountries = computed(() => {
 
 <template>
   <main class="p-6 max-w-7xl mx-auto">
+	  <h1 class="text-3xl md:text-5xl text-center md:text-left font-bold mb-8">
+		  Countries
+	  </h1>
+
     <div class="flex flex-col md:flex-row gap-4 mb-10 justify-between">
       <input
 	      v-model="search"
